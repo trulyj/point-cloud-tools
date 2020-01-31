7402bceb506c82dfe8bef5ec8d501f3e00a46bb6
trulyj <trulyj.sof@gmail.com>
2020-01-31 12:50:15 -0500

    Update comments on pickledata.py

--- pickledata.py
+++ pickledata.py
@@ -1,5 +1,5 @@
 #Converts unlabeled point cloud scans scans into pickle files for use with PointNet++. Uses as input point cloud scans in .txt form.
-#The locations of these files are indicated by robo_paths.txt, which contains the paths to the directories where the files are stored. 
+#The locations of these files are indicated by paths.txt, which contains the paths to the directories where the files are stored. 
 
 
 import glob
